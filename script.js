2266105360e9a279f5a5b5004dca263f9a67398b
Quan <quanp155@gmail.com>
2020-12-03 15:52:14 -0600

    Created functions for 4 basic math operators

---
+++ script.js
@@ -0,0 +1,22 @@
+var num1;
+var num2;
+
+function add(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+function subtract(num1, num2) {
+    var diff = num1 - num2;
+    return diff;
+}
+
+function multiply(num1, num2) {
+    var prod = num1 * num2;
+    return prod;
+}
+
+function divide(num1, num2) {
+    var quot = num1 / num2;
+    return quot;
+} 
