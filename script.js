cbf45f688f1bd247ee65e0d2fbb0f0d9fa9d71a6
Quan <quanp155@gmail.com>
2020-12-17 15:21:09 -0600

    Created equal function to utilize the 4 basic math operators to return a value

--- script.js
+++ script.js
@@ -1,61 +1,8 @@
-function add() {
-    
-}
-
-function subtract(num1, num2) {
-    var diff = num1 - num2;
-    return diff;
-}
-
-function multiply(num1, num2) {
-    var prod = num1 * num2;
-    return prod;
-}
-
-function divide(num1, num2) {
-    var quot = num1 / num2;
-    return quot;
-}
-var operator;
-
-function operate(operator) {
-    switch (operator) {
-        case "+":
-            add(num1, num2);
-            break;
-        case "-":
-            subtract(num1, num2);
-            break;
-        case "*":
-            multiply(num1, num2);
-            break;
-        case "/":
-            divide(num1, num2);  
-    }
-}
-
 function clearContent() {
     document.getElementById('display').innerText = "";
 }
 
-var clicked = false;
-
-// document.getElementById('addition').onclick = function() {
-//     clicked = true;
-// }
-
-// document.getElementById('subtract').onclick = function() {
-//     clicked = true;
-// }
-
-// document.getElementById('multiply').onclick = function() {
-//     clicked = true;
-// }
-
-// document.getElementById('divide').onclick = function() {
-//     clicked = true;
-// }
-  
+var clicked = false;  
 
 function displayVal(value) {
     if (document.getElementById('display').innerHTML.length < 11) {
@@ -69,15 +16,67 @@ function displayVal(value) {
         document.getElementById('display').innerHTML += value;
         clicked = false;
     }
-
 }
 
 function numSave(value) {
     num1 = document.getElementById('display').textContent;
     num1 = parseInt(num1);
 
-    var opClicked = value;
-    console.log(opClicked);
-    
+    opClicked = value;
+
     clicked = true;
+
+}
+
+var opClicked;
+
+function equal() {
+    if (typeof(num1) == 'number') {
+        num2 = document.getElementById('display').textContent;
+        num2 = parseInt(num2);
+    }
+    operate(opClicked);
+}
+
+
+
+function add(num1, num2) {
+    var sum = num1 + num2;
+    document.getElementById('display').innerHTML = sum;
+    sum = num1;
+}
+
+function subtract(num1, num2) {
+    var diff = num1 - num2;
+    document.getElementById('display').innerHTML = diff;
+    diff = num1;
+}
+
+function multiply(num1, num2) {
+    var prod = num1 * num2;
+    document.getElementById('display').innerHTML = prod;
+    prod = num1;
+}
+
+function divide(num1, num2) {
+    var quot = num1 / num2;
+    document.getElementById('display').innerHTML = quot;
+    prod = num1;
+}
+
+function operate(opClicked) {
+    switch (opClicked) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);  
+    }
 }
